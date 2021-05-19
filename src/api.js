dc760083c49a31d8dfc80695ff073ba47f34a28c
sabine-mansour <sabine_mansour@icloud.com>
2021-05-19 20:24:15 +0200

    Code correction

--- src/api.js
+++ src/api.js
@@ -2,27 +2,6 @@ import { mockData } from './mock-data';
 import axios from 'axios';
 import NProgress from 'nprogress';
 
-const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
-
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const results = await axios.get(
-        "https://hd58hdacgj.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const { authUrl } = results.data
-      return (window.location.href = authUrl)
-    }
-    return code && getAccessToken(code);
-  }
-  return accessToken;
-}
-
 const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
