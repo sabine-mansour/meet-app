a1fe1bd3b4018917580c3ada02356c100c496453
sabine-mansour <sabine_mansour@icloud.com>
2021-05-31 14:52:37 +0200

    PWA conversion

--- src/index.js
+++ src/index.js
@@ -10,5 +10,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+//serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 atatus.config('1a15c75808324fff86363ad0beaa4f64').install();
