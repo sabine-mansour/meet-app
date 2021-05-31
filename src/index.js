90cf3abb7d2c20657508546a3da33ff381bf5125
sabine-mansour <sabine_mansour@icloud.com>
2021-05-31 22:35:24 +0200

    PWA

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorker from './serviceWorkerRegistration';
 import * as atatus from 'atatus-spa';
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -11,5 +11,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 //serviceWorkerRegistration.unregister();
-serviceWorkerRegistration.register();
+serviceWorker.register();
 atatus.config('1a15c75808324fff86363ad0beaa4f64').install();
