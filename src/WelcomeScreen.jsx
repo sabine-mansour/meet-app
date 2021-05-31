76a7587ab226708dfee65960448430a0b6064065
sabine-mansour <sabine_mansour@icloud.com>
2021-06-01 00:02:30 +0200

    Welcome Screen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -18,13 +18,14 @@ function WelcomeScreen(props) {
                 src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                 alt="Google sign-in"
               />
+
+              <button onClick={() => { props.getAccessToken() }}
+                rel="nofollow noopener"
+                class="btn-text"
+              >
+                <b>Sign in with google</b>
+              </button>
             </div>
-            <button onClick={() => { props.getAccessToken() }}
-              rel="nofollow noopener"
-              class="btn-text"
-            >
-              <b>Sign in with google</b>
-            </button>
           </div>
         </div>
         <a
